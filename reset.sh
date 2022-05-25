682f9e6c0a4673f00de3777f826f9cf134674255
Stanislav <skandyrov@gmail.com>
2022-05-25 09:14:42 +0500

    Add files via upload
    reset some idea

---
+++ reset.sh
@@ -0,0 +1,3 @@
+rm /home/hostuser/.config/JetBrains/IntelliJIdea2020.3/eval/idea*.evaluation.key && \
+sed -i '/evlsprt/d' /home/hostuser/.config/JetBrains/IntelliJIdea2020.3/options/other.xml && \
+rm -rf ~/.java/.userPrefs/jetbrains
